e1ba47806d67ae6e00aca2f0a1b43ce15448bded
Agw-a <lizagwa7@gmail.com>
2023-01-05 20:13:45 +0000

    Fixing comments

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-"""add function"""
+"""add function that accepts two parameters a and b both floats
+    return a floating type integer.
+"""
 
 
 def add(a: float, b: float) -> float:
+    '''Addition function'''
     return a + b
