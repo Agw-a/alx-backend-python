d5327f47292361c4b0ca65e715032916d22e3ac7
Agw-a <lizagwa7@gmail.com>
2023-01-05 20:19:08 +0000

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""function that takes two string parameters and returns a concated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concated string"""
+    return str1 + str2
