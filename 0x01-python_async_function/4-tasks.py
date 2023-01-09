07ee172ecb2f1215486792b74cddd8269af72e08
Agw-a <lizagwa7@gmail.com>
2023-01-09 21:46:57 +0000

    Fixing imports

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,15 +2,15 @@
 
 '''Tak 4 module
 '''
+
 import asyncio
-from typing import
+from typing import List
 
-task_wait_n = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_await_n(n: int, max_delay: int) -> List[float]:
-    '''return a list of delayed tasks
-    '''
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns list of delayed async tasks"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delayed_tasks = [await task for task in asyncio.as_completed(tasks)]
     return delayed_tasks
