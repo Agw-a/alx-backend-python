c19f50319d10c9e7f94c7fe83054ee0caf845a4b
Agw-a <lizagwa7@gmail.com>
2023-01-30 13:45:27 +0300

    lint

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 ''' Test suite for various cases
 '''
 import unittest
-from parameterized import parameterized, parameterized_class, param
+from parameterized import parameterized
 from utils import access_nested_map
 from typing import Mapping, Sequence, Any
 
@@ -13,8 +13,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"),  2)
     ])
-    def test_access_nested_map(self, nested_map: Mapping,
-                               path: Sequence, result: Any) -> None:
+    def test_access_nested_map(
+            self, nested_map: Mapping,
+            path: Sequence,
+            result: Any) -> None:
         ''' Test return value of utils.access_nested_map() '''
         self.assertEqual(access_nested_map(nested_map, path), result)
 
@@ -22,13 +24,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
     ])
-    def test_access_nested_map_exception(self, nested_map: Mapping,
-                                         path: Sequence,
-                                         key: Exception) -> None:
+    def test_access_nested_map_exception(
+            self, nested_map: Mapping,
+            path: Sequence,
+            key: Exception) -> None:
         '''Tests exception raised by utils.access_nested_map'''
         with self.assertRaises(key):
             access_nested_map(nested_map, path)
-
-
-class TestGetJson(unittest.TestCase):
-    pass
