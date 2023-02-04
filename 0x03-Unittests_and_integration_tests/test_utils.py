662e259502a5118ed1352387c2988ab5f62552c8
Agw-a <lizagwa7@gmail.com>
2023-02-04 09:47:08 +0300

    test_get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,14 @@
 ''' Test suite for various cases
 '''
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
-from typing import Mapping, Sequence, Any
+from utils import access_nested_map, get_json
+from typing import Mapping, Sequence, Any, Dict
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    '''Tests nested map'''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -31,3 +33,20 @@ class TestAccessNestedMap(unittest.TestCase):
         '''Tests exception raised by utils.access_nested_map'''
         with self.assertRaises(key):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self,
+                      test_url: str,
+                      test_payload: Dict) -> None:
+        '''test that utils.get_json returns the expected result.
+        '''
+        #  Patch requests.get
+        configs = {"json.return_value": test_payload}
+        with patch("requests.get", return_value=Mock(**configs)) as query:
+            self.assertEqual(get_json(test_url), test_payload)
+            self.asstert_called_once(test_url)
